f51c95f9a51d5260a0f6b972f8894d383c164f53
Rajesh E <rraj241587@gmail.com>
2024-11-03 06:32:44 +0530

    API update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import google.generativeai as genai
 
 st.title("welcome to gemini chat")
 
-genai.configure(api_key="AIzaSyB_S-zFLd6AGKnMIC2ugeq0X0t7jeLyepU")  #to get api-key https://aistudio.google.com/app/apikey
+genai.configure(api_key="YOUR_API")  #to get api-key https://aistudio.google.com/app/apikey
 
 text = st.text_input("enter your question")
 
